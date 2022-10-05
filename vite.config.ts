f667dfaee73c6246d108199dc264153e7b14c10b
Marcio Barrientos <marcio.barrientos@gmail.com>
2022-10-05 10:50:31 -0600

    Proxy de API para evitar problemas de CORS

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5&action=upload&format=json'
+    }
+  }
 })
